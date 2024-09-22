f227ffdca9b5fffe6c283b5bc78f650d78e0b0d2
Senie Calalang <senie.c88@gmail.com>
2024-09-22 17:54:45 +0900

    WIP: update project details and add typewriter effect in banner

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,35 @@
 // This is where it all goes :)
+
+// Array of strings to type out
+const items = ["I'm a developer", "I am a designer", "I love coding"];
+
+// Function to type out each character
+function typeWriter(text, i, callback) {
+    if (i < text.length) {
+        document.getElementById("typewriter").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+        setTimeout(function() {
+            typeWriter(text, i + 1, callback);
+        }, 100);
+    } else if (typeof callback == 'function') {
+        setTimeout(callback, 700);
+    }
+}
+
+// Function to start the typing effect
+function startTextAnimation(i) {
+    if (typeof items[i] == 'undefined') {
+        setTimeout(function() {
+            startTextAnimation(0);
+        }, 20000);
+    }
+    if (i < items.length) {
+        typeWriter(items[i], 0, function() {
+            startTextAnimation(i + 1);
+        });
+    }
+}
+
+// Start the typing effect
+document.addEventListener('DOMContentLoaded', function(event) {
+    startTextAnimation(0);
+});
