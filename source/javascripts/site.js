c8826eafd9d3a41a871f33d3e9e03bd95be9ca3f
Senie Calalang <senie.c88@gmail.com>
2024-10-19 14:30:00 +0900

    feat: added toggleMenu function to site.js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -37,3 +37,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // Start the typing effect
     startTextAnimation(0);
 });
+
+function toggleMenu() {
+    const mobileMenu = document.getElementById("mobile-menu");
+    mobileMenu.style.display = mobileMenu.style.display === "block" ? "none" : "block";
+  }
